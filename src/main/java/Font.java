0a4e3f55e38806054d5f1bd005fe8db7cf9725b0
Per Arneng <per.arneng@scalebit.com>
2015-03-15 09:57:27 +0100

    font tool

---
+++ src/main/java/Font.java
@@ -0,0 +1,46 @@
+
+class Font {
+
+	public static void main(String[] args) {
+
+		System.out.println("//.");
+		System.out.println(0b0000000000000001);
+
+		System.out.println("//<space>");
+		System.out.println(0b0000000000000000);
+			
+		System.out.println("//1");
+		System.out.println(0b0010010010010010);
+				
+		System.out.println("//2");
+		System.out.println(0b0111001111100111);
+			
+		System.out.println("//3");
+		System.out.println(0b0111001111001111);
+			
+		System.out.println("//4");
+		System.out.println(0b0101101111001001);
+			
+		System.out.println("//5");
+		System.out.println(0b0111100111001111);
+			
+		System.out.println("//6");
+		System.out.println(0b0111100111101111);
+			
+		System.out.println("//7");
+		System.out.println(0b0111001001001001);
+			
+		System.out.println("//8");
+		System.out.println(0b0111101111101111);
+			
+		System.out.println("//9");
+		System.out.println(0b0111101111001111);
+			
+		System.out.println("//0");
+		System.out.println(0b0111101101101111);
+			
+		System.out.println("//default");
+		System.out.println(0b0101010101010101);
+	}
+
+}
